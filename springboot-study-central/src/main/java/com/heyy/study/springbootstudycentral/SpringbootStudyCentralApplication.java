eb81d95714f197d5718a526c14d476be37a5ba94
Breeze <1091524653@qq.com>
2019-05-08 20:30:30 +0800

    add springboot-study-centrdal modul

---
+++ springboot-study-central/src/main/java/com/heyy/study/springbootstudycentral/SpringbootStudyCentralApplication.java
@@ -0,0 +1,13 @@
+package com.heyy.study.springbootstudycentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyCentralApplication.class, args);
+	}
+
+}
---
+++ springboot-study-central/src/test/java/com/heyy/study/springbootstudycentral/SpringbootStudyCentralApplicationTests.java
@@ -0,0 +1,16 @@
+package com.heyy.study.springbootstudycentral;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootStudyCentralApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
