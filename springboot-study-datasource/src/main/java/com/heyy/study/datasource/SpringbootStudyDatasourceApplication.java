9b9b2645c2d4c2b15d70ac00840b431ebee7d5b6
Breeze <1091524653@qq.com>
2019-05-09 13:23:26 +0800

    add other modul for springboot study project

---
+++ springboot-study-datasource/src/main/java/com/heyy/study/datasource/SpringbootStudyDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.heyy.study.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyDatasourceApplication.class, args);
+	}
+
+}
