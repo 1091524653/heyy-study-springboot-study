3895e5f2fd0d8abbf7ab6c68b7ebc9e16c44f57d
Breeze <1091524653@qq.com>
2019-05-11 00:03:18 +0800

    add new project springboot-study-datasource

---
+++ springboot-study-shared/src/main/java/heyy/study/common/constants/RegExpConstants.java
@@ -0,0 +1,11 @@
+package heyy.study.common.constants;
+
+/**
+ * @Classname RegExpConstants
+ * @Description TODO
+ * @Date 2019/5/9 20:49
+ * @Created by Breeze
+ */
+public class RegExpConstants {
+    public static final String REGEX_NAME = "([A-Za-z0-9\\u4e00-\\u9fa5]|-){1,}";
+}
